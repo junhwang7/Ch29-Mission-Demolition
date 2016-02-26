cf993ccb0fcc34affa59883aae03e5ce962c6069
junhwang7 <kyuthwang@nbp-86-163.nbp.ruw.rutgers.edu>
2016-02-25 19:53:21 -0500

    Commit
    Two Levels

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -59,6 +59,7 @@ public class Slingshot : MonoBehaviour {
 			projectile.GetComponent<Rigidbody> ().velocity = -mouseDelta * velocityMult;
 			FollowCam.S.poi = projectile;
 			projectile = null;
+			MissionDemolition.ShotFired ();
 		}
 	}
 }
